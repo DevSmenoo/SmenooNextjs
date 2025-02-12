d78ee9ef78fb7b2d48236afb3c958e5c983fd4c2
DevSmenoo <dev@smenoo.it>
2025-02-12 11:17:46 +0100

    mod next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    output: 'standalone', // Aiuta con il deploy su Vercel
-    experimental: {
-      serverActions: true, // Se usi server components
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  reactStrictMode: true,
+  output: "standalone",
+};
+
+export default nextConfig;
