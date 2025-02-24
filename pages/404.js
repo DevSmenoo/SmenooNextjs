60b57bfc9906e777ff93f345802f53f17ffac063
DevSmenoo <dev@smenoo.it>
2025-02-24 11:23:24 +0100

    add 404 page

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+// pages/404.js
+
+export default function Custom404() {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '100px' }}>
+        <h1>404 - Pagina Non Trovata</h1>
+        <p>Sembra che la pagina che cerchi non esista. Prova altrove!</p>
+        <a href="/" style={{ color: '#0070f3', textDecoration: 'underline' }}>
+          Torna alla Home
+        </a>
+      </div>
+    );
+  }
+  
